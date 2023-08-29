11be347a9e38853edc5937f4767cd3b736b318c2
w9i <m.p1nk@yandex.com>
2023-08-29 12:21:26 +0300

    lesson 7

--- js/ex1.js
+++ js/ex1.js
@@ -1,8 +1,9 @@
 "use strict";
 
-let a, b, c = 10,
-    d = 10;
-console.log(a, b, c, d);
-a = c++;  // <1>
-b = ++d;  // <2>
-console.log(a, b, c, d);  // 10 11 11 11
+let S=0, M=10, N=5;
+all: for (let i=1; i<=N; i++)
+    for (let j=1; j<=M; j++) {
+        if (j==5) break all;
+        S += i*j;
+    }
+console.log('S=' + S);  // S=10
